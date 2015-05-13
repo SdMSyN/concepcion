2614a41460f4024c76cf712227532c2ba26d5e8c
GianBros <GianBros@GianBros-PC.lan>
2015-05-13 14:49:27 -0500

    Bases de Datos: almacenes, categorias, costales, perfiles, productos, sobrantes, tiendas, usuarios, ventas_info, ventas_prod.

---
+++ db_schemas/concepcion.sql
@@ -0,0 +1,244 @@
+-- phpMyAdmin SQL Dump
+-- version 4.0.9
+-- http://www.phpmyadmin.net
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 13-05-2015 a las 21:47:50
+-- Versión del servidor: 5.6.14
+-- Versión de PHP: 5.5.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Base de datos: `concepcion`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `almacenes`
+--
+
+CREATE TABLE IF NOT EXISTS `almacenes` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `producto_id` int(11) NOT NULL,
+  `cantidad` int(11) NOT NULL,
+  `tienda_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `producto_id` (`producto_id`),
+  KEY `tienda_id` (`tienda_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `categorias`
+--
+
+CREATE TABLE IF NOT EXISTS `categorias` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(20) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `costales`
+--
+
+CREATE TABLE IF NOT EXISTS `costales` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `tienda_id` int(11) NOT NULL,
+  `cantidad` int(11) NOT NULL,
+  `fecha` date NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `tienda_id` (`tienda_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `perfiles`
+--
+
+CREATE TABLE IF NOT EXISTS `perfiles` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `perfil` varchar(15) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `productos`
+--
+
+CREATE TABLE IF NOT EXISTS `productos` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(50) NOT NULL,
+  `precio` decimal(10,0) NOT NULL,
+  `img` varchar(20) NOT NULL,
+  `descripcion` varchar(100) NOT NULL,
+  `activo` tinyint(1) NOT NULL,
+  `codigo_barras` varchar(20) NOT NULL,
+  `pan_frio` tinyint(1) NOT NULL,
+  `categoria_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `categoria_id` (`categoria_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `sobrantes`
+--
+
+CREATE TABLE IF NOT EXISTS `sobrantes` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `producto_id` int(11) NOT NULL,
+  `tienda_ida` int(11) NOT NULL,
+  `cantidad` int(11) NOT NULL,
+  `fecha` date NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `producto_id` (`producto_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tiendas`
+--
+
+CREATE TABLE IF NOT EXISTS `tiendas` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(50) NOT NULL,
+  `direccion` varchar(100) NOT NULL,
+  `rfc` varchar(13) NOT NULL,
+  `cp` int(5) NOT NULL,
+  `tel` varchar(15) NOT NULL,
+  `num_sess` int(2) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE IF NOT EXISTS `usuarios` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(50) NOT NULL,
+  `ap` varchar(30) NOT NULL,
+  `am` varchar(30) NOT NULL,
+  `password` int(11) NOT NULL,
+  `perfil_id` int(11) NOT NULL,
+  `direccion` varchar(100) DEFAULT NULL,
+  `num_int` varchar(10) DEFAULT NULL,
+  `num_ext` varchar(10) DEFAULT NULL,
+  `colonia` varchar(50) DEFAULT NULL,
+  `municipio` varchar(40) DEFAULT NULL,
+  `telefono` varchar(15) DEFAULT NULL,
+  `celular` varchar(15) DEFAULT NULL,
+  `created` date NOT NULL,
+  `updated` date NOT NULL,
+  `fec_nac` date DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `perfil_id` (`perfil_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `ventas_info`
+--
+
+CREATE TABLE IF NOT EXISTS `ventas_info` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `usuario_id` int(11) NOT NULL,
+  `tienda_id` int(11) NOT NULL,
+  `fecha` date NOT NULL,
+  `hora` varchar(5) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `usuario_id` (`usuario_id`),
+  KEY `tienda_id` (`tienda_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `ventas_prod`
+--
+
+CREATE TABLE IF NOT EXISTS `ventas_prod` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `producto_id` int(11) NOT NULL,
+  `venta__info_id` int(11) NOT NULL,
+  `cantidad` int(3) NOT NULL,
+  `costo_unitario` varchar(10) NOT NULL,
+  `costo_total` varchar(10) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `producto_id` (`producto_id`),
+  KEY `venta__info_id` (`venta__info_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `almacenes`
+--
+ALTER TABLE `almacenes`
+  ADD CONSTRAINT `almacenes_ibfk_2` FOREIGN KEY (`tienda_id`) REFERENCES `tiendas` (`id`),
+  ADD CONSTRAINT `almacenes_ibfk_1` FOREIGN KEY (`producto_id`) REFERENCES `productos` (`id`);
+
+--
+-- Filtros para la tabla `costales`
+--
+ALTER TABLE `costales`
+  ADD CONSTRAINT `costales_ibfk_1` FOREIGN KEY (`tienda_id`) REFERENCES `tiendas` (`id`);
+
+--
+-- Filtros para la tabla `productos`
+--
+ALTER TABLE `productos`
+  ADD CONSTRAINT `productos_ibfk_1` FOREIGN KEY (`categoria_id`) REFERENCES `categorias` (`id`);
+
+--
+-- Filtros para la tabla `sobrantes`
+--
+ALTER TABLE `sobrantes`
+  ADD CONSTRAINT `sobrantes_ibfk_1` FOREIGN KEY (`producto_id`) REFERENCES `productos` (`id`);
+
+--
+-- Filtros para la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD CONSTRAINT `usuarios_ibfk_1` FOREIGN KEY (`perfil_id`) REFERENCES `perfiles` (`id`);
+
+--
+-- Filtros para la tabla `ventas_info`
+--
+ALTER TABLE `ventas_info`
+  ADD CONSTRAINT `ventas_info_ibfk_2` FOREIGN KEY (`tienda_id`) REFERENCES `tiendas` (`id`),
+  ADD CONSTRAINT `ventas_info_ibfk_1` FOREIGN KEY (`usuario_id`) REFERENCES `usuarios` (`id`);
+
+--
+-- Filtros para la tabla `ventas_prod`
+--
+ALTER TABLE `ventas_prod`
+  ADD CONSTRAINT `ventas_prod_ibfk_2` FOREIGN KEY (`venta__info_id`) REFERENCES `ventas_info` (`id`),
+  ADD CONSTRAINT `ventas_prod_ibfk_1` FOREIGN KEY (`producto_id`) REFERENCES `productos` (`id`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
