0ba0f20298ef22fd4e52e8bdc0fd1207178f808c
GianBros <GianBros@GianBros-PC.lan>
2015-05-20 01:15:02 -0500

    Modificación de Tabla Tienda

--- db_schemas/queries.sql
+++ db_schemas/queries.sql
@@ -1,1 +1,4 @@
 ALTER TABLE  `usuarios` ADD  `activo` TINYINT NOT NULL ; /*Añade el campo 'activo' para comprobar si un usuario aún trabaja. */
+
+/* Añade el campo 'activa' a la tabla tiendas */
+ALTER TABLE  `tiendas` ADD  `activa` TINYINT NOT NULL ;
