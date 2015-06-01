e1ef522a172487bb1cc9e57879aeef11e6579630
Alex <Alex@192.168.1.106>
2015-06-01 15:35:29 -0500

    ajuste menu

--- menu.php
+++ menu.php
@@ -3,38 +3,56 @@
     <!-- Brand and toggle get grouped for better mobile display -->
     <div class="navbar-header">
       <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-	<span class="sr-only">Menu</span>
-	<span class="icon-bar"></span>
-	<span class="icon-bar"></span>
-	<span class="icon-bar"></span>
+        <span class="sr-only">Menu</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="index_admin.php">Inicio</a>
+      <?php
+      if (isset($_SESSION['perfil']) && $_SESSION['perfil'] == "1" ) {
+        ?>
+          <a class="navbar-brand" href="index_admin.php">Inicio</a>
+        <?php
+      }elseif (isset($_SESSION['perfil']) && $_SESSION['perfil'] == "0" ){
+        ?>
+          <a class="navbar-brand" href="form_sales.php">Inicio</a>
+        <?php
+      }else{
+        ?>
+          <a class="navbar-brand" href="form_login_store.php">Inicio</a>
+        <?php
+      }
+      ?>      
     </div>
 
     <!-- Collect the nav links, forms, and other content for toggling -->
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav">
-	<?php
-	if ($_SESSION['perfil'] == "1") {
-	  ?>
-	  <li><a href="form_select_stock_2.php">Almacen</a></li>
-	  <li><a href="form_select_product.php">Productos</a></li>
-	  <li><a href="form_select_user.php">Usuarios</a></li>
-	  <li><a href="form_select_store.php">Tiendas</a></li>
-	  <li><a href="form_select_category.php">Categorías</a></li>
-	  <?php
-	}//fin else sesión
-	?>
+        <?php
+        if (isset($_SESSION['perfil']) && $_SESSION['perfil'] == "1") {
+          ?>
+          <li><a href="form_select_stock_2.php">Almacen</a></li>
+          <li><a href="form_select_product.php">Productos</a></li>
+          <li><a href="form_select_user.php">Usuarios</a></li>
+          <li><a href="form_select_store.php">Tiendas</a></li>
+          <li><a href="form_select_category.php">Categorías</a></li>
+          <?php
+        } elseif (isset($_SESSION['perfil']) && $_SESSION['perfil'] == "0") {
+          ?>
+          <li><a href="form_select_stock_2.php">Hola mundo</a></li>
+          <?php
+        }
+        ?>
 
 
       </ul>
       <ul class="nav navbar-nav navbar-right">
-	<?php
-	if (isset($_SESSION['sessA'])) {
-	  echo '<li class="no-a user-name">Bienvenido ' . $_SESSION['userName'] . '</li>';
-	  echo '<li><a href="controllers/proc_destroy_login_admin.php">Cerrar Sesión</a></li>';
-	}
-	?>
+        <?php
+        if (isset($_SESSION['sessA'])) {
+          echo '<li class="no-a user-name">Bienvenido ' . $_SESSION['userName'] . '</li>';
+          echo '<li><a href="controllers/proc_destroy_login_admin.php">Cerrar Sesión</a></li>';
+        }
+        ?>
       </ul>
     </div><!-- /.navbar-collapse -->
   </div><!-- /.container-fluid -->
