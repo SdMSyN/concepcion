122088e8525bc18478733a79d5fd317ee811e4e7
GianBros <GianBros@GianBros-PC.lan>
2015-06-03 00:18:05 -0500

    Corrección error

--- controllers/set_sale.php
+++ controllers/set_sale.php
@@ -2,15 +2,14 @@
     include ('../config/conexion.php');
     include ('../config/variables.php');
   
-    $i=0; $cad ='';
+    $i=0; $cad ='<table border="2"><tr><td>Id</td><td>Precio U.</td><td>Cantidad</td><td>Precio F.</td></tr>';
     for($i=0; $i<count($_POST['id']); $i++){
-        $cad.=$_POST['id'][$i].'--';
-        $cad.=$_POST['inputPrecioU'][$i].'--';
-        $cad.=$_POST['inputCant'][$i].'--';
-        $cad.=$_POST['inputPrecioF'][$i].'--';
-        $cad.='<br>';
+        $cad.='<td>'.$_POST['id'][$i].'</td>';
+        $cad.='<td>'.$_POST['inputPrecioU'][$i].'</td>';
+        $cad.='<td>'.$_POST['inputCant'][$i].'</td>';
+        $cad.='<td>'.$_POST['inputPrecioF'][$i].'</td></tr>';
     }
-    echo $cad.'=='.$_POST['inputTotal'];
+    echo $cad.'<tr><td colspan="3">Total</td><td>'.$_POST['inputTotal'].'</td></tr></table>';
     /*var_dump($_POST['id']).'<br>';
     var_dump($_POST['inputPrecioU']).'<br>';
     var_dump($_POST['inputCant']).'<br>';
