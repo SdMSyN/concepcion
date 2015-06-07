57c89e12f4504bacb6d47882228870f6c6546de9
GianBros <GianBros@GianBros-PC.lan>
2015-06-07 14:59:02 -0500

    Corrección query

--- controllers/login_admin.php
+++ controllers/login_admin.php
@@ -4,7 +4,7 @@
     $user = $_POST['inputAdmin'];
     $passAdmin = $_POST['inputPassAdmin'];
     
-    $sqlGetAdmin="SELECT * FROM $tUser WHERE user='$user' AND password='$passAdmin' ";
+    $sqlGetAdmin="SELECT * FROM $tUser WHERE BINARY user='$user' AND BINARY password='$passAdmin' ";
     $resGetAdmin=$con->query($sqlGetAdmin);
     if($resGetAdmin->num_rows > 0){
         $rowGetAdmin=$resGetAdmin->fetch_assoc();
