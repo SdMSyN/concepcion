024c2b96761020876291a647895535dc13fc13ad
Alex <Alex@192.168.1.69>
2015-06-16 23:43:31 -0500

    cambios de estilo reservacones

--- controllers/set_sale.php
+++ controllers/set_sale.php
@@ -1,7 +1,7 @@
 <html lang="en">
   <head>
     <?php
-        include ('../config/variables.php');
+    include ('../config/variables.php');
     ?>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -13,7 +13,7 @@
 
     <!-- Bootstrap -->
     <link href="../assets/css/application.css" rel="stylesheet">
-    
+
     <!-- jQuery -->
     <script src="../assets/js/jquery.min.js"></script>
     <script src="../assets/js/bootstrap.min.js"></script>
@@ -38,197 +38,199 @@
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
   </head>
-  
+
   <body>
-      
-      <nav class="navbar navbar-default">
-  <div class="container-fluid">
-    <!-- Brand and toggle get grouped for better mobile display -->
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-        <span class="sr-only">Menu</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
+
+    <nav class="navbar navbar-default">
+      <div class="container-fluid">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+            <span class="sr-only">Menu</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <?php
+          if (isset($_SESSION['perfil']) && $_SESSION['perfil'] == "1") {
+            if (isset($_SESSION['sessA'])) {
+              ?>
+              <a class="navbar-brand" href="../index_admin.php">Inicio</a>
+              <?php
+            } else {
+              ?>
+              <a class="navbar-brand" href="../form_sales.php">Inicio</a>
+              <a class="navbar-brand" href="../form_orders.php">Pedidos</a>
+              <?php
+            }
+          } elseif (isset($_SESSION['perfil']) && $_SESSION['perfil'] == "3" && isset($_SESSION['sess'])) {
+            ?>
+            <a class="navbar-brand" href="../form_sales.php">Inicio</a>
+            <a class="navbar-brand" href="../form_orders.php">Pedidos</a>
+            <?php
+          } else {
+            ?>
+            <a class="navbar-brand" href="../form_sales.php">Inicio</a>
+            <a class="navbar-brand" href="../form_orders.php">Pedidos</a>
+            <?php
+          }
+          ?>      
+        </div>
+
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+          <ul class="nav navbar-nav">
+            <?php
+            if (isset($_SESSION['perfil']) && $_SESSION['perfil'] == "1" && isset($_SESSION['sessA'])) {
+              ?>
+              <li><a href="../form_select_stock.php">Almacen</a></li>
+              <li><a href="../form_select_product.php">Productos</a></li>
+              <li><a href="../form_select_user.php">Usuarios</a></li>
+              <li><a href="../form_select_store.php">Tiendas</a></li>
+              <li><a href="../form_select_category.php">Categorías</a></li>
+              <li><a href="../form_select_subcategory.php">Subcategorías</a></li>
+              <li><a href="../form_select_report.php">Reportes</a></li>
+              <?php
+            } elseif (isset($_SESSION['perfil']) && $_SESSION['perfil'] == "3" && isset($_SESSION['sess'])) {
+              ?>
+              <!--          <li><a href="form_select_stock_2.php">Hola mundo</a></li>-->
+              <?php
+            }
+            ?>
+
+
+          </ul>
+          <ul class="nav navbar-nav navbar-right">
+            <?php
+            if (isset($_SESSION['perfil'])) {
+              echo '<li class="no-a user-name">Bienvenido ' . $_SESSION['userName'] . '</li>';
+              echo '<li><a href="proc_destroy_login_admin.php">Cerrar Sesión</a></li>';
+            }
+            ?>
+          </ul>
+        </div><!-- /.navbar-collapse -->
+      </div><!-- /.container-fluid -->
+    </nav>
+
+    <div class="row" >
+     
       <?php
-      if (isset($_SESSION['perfil']) && $_SESSION['perfil'] == "1") {
-        if (isset($_SESSION['sessA'])) {
-          ?>
-          <a class="navbar-brand" href="../index_admin.php">Inicio</a>
-          <?php
+      include ('../config/conexion.php');
+      include ('../config/variables.php');
+
+      $idStore = $_POST['idStore'];
+      $idUser = $_POST['idUser'];
+
+      (isset($_POST['inputDonacion'])) ? $dona = $_POST['inputDonacion'] : $dona = "false";
+      ($dona != "on") ? $recibido = $_POST['inputRecibido'] : $recibido = "0.00";
+      $total = $_POST['inputTotal'];
+      ($dona != "on") ? $cambio = $_POST['inputCambio'] : $cambio = "0.00";
+      ($dona == "on") ? $idAdmin = $_POST['inputAdmin'] : $idAdmin = "";
+      $cad = '';
+      //Obtenemos datos de la tienda
+      //echo '<h2>'.$dona.'</h2>';
+
+      $donarAct = true;
+      if ($dona == "on") {
+        $sqlGetUserDon = "SELECT id FROM $tUser WHERE password='$idAdmin' AND perfil_id='1' ";
+        $resGetUserDon = $con->query($sqlGetUserDon);
+        if ($resGetUserDon->num_rows > 0) {
+          $rowGetUserDon = $resGetUserDon->fetch_assoc();
+          $idUser = $rowGetUserDon['id'];
         } else {
-          ?>
-          <a class="navbar-brand" href="../form_sales.php">Inicio</a>
-          <a class="navbar-brand" href="../form_orders.php">Pedidos</a>
-          <?php
+          $donarAct = false;
         }
-      } elseif (isset($_SESSION['perfil']) && $_SESSION['perfil'] == "3" && isset($_SESSION['sess'])) {
-        ?>
-        <a class="navbar-brand" href="../form_sales.php">Inicio</a>
-        <a class="navbar-brand" href="../form_orders.php">Pedidos</a>
-        <?php
-      } else {
-        ?>
-        <a class="navbar-brand" href="../form_login_store.php">Inicio</a>
-        <a class="navbar-brand" href="../form_orders.php">Pedidos</a>
-        <?php
       }
-      ?>      
-    </div>
-
-    <!-- Collect the nav links, forms, and other content for toggling -->
-    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-      <ul class="nav navbar-nav">
-        <?php
-        if (isset($_SESSION['perfil']) && $_SESSION['perfil'] == "1" && isset($_SESSION['sessA'])) {
-          ?>
-          <li><a href="../form_select_stock.php">Almacen</a></li>
-          <li><a href="../form_select_product.php">Productos</a></li>
-          <li><a href="../form_select_user.php">Usuarios</a></li>
-          <li><a href="../form_select_store.php">Tiendas</a></li>
-          <li><a href="../form_select_category.php">Categorías</a></li>
-          <li><a href="../form_select_subcategory.php">Subcategorías</a></li>
-          <li><a href="../form_select_report.php">Reportes</a></li>
-          <?php
-        } elseif (isset($_SESSION['perfil']) && $_SESSION['perfil'] == "3" && isset($_SESSION['sess'])) {
-          ?>
-          <!--          <li><a href="form_select_stock_2.php">Hola mundo</a></li>-->
-          <?php
-        }
-        ?>
-
-
-      </ul>
-      <ul class="nav navbar-nav navbar-right">
-        <?php
-        if (isset($_SESSION['perfil'])) {
-          echo '<li class="no-a user-name">Bienvenido ' . $_SESSION['userName'] . '</li>';
-          echo '<li><a href="proc_destroy_login_admin.php">Cerrar Sesión</a></li>';
-        }
-        ?>
-      </ul>
-    </div><!-- /.navbar-collapse -->
-  </div><!-- /.container-fluid -->
-</nav>
-      
-      <div class="row">
-<?php
-    include ('../config/conexion.php');
-    include ('../config/variables.php');
-  
-    $idStore=$_POST['idStore'];
-    $idUser=$_POST['idUser'];
-    
-    (isset($_POST['inputDonacion'])) ? $dona=$_POST['inputDonacion'] : $dona="false";
-    ($dona!="on") ? $recibido=$_POST['inputRecibido'] : $recibido="0.00";
-    $total=$_POST['inputTotal'];
-    ($dona!="on") ? $cambio=$_POST['inputCambio'] : $cambio="0.00";
-    ($dona=="on") ? $idAdmin=$_POST['inputAdmin'] : $idAdmin="";
-    $cad='';
-    //Obtenemos datos de la tienda
-    //echo '<h2>'.$dona.'</h2>';
-    
-    $donarAct=true;
-    if($dona=="on"){
-        $sqlGetUserDon="SELECT id FROM $tUser WHERE password='$idAdmin' AND perfil_id='1' ";
-        $resGetUserDon=$con->query($sqlGetUserDon);
-        if($resGetUserDon->num_rows >0){
-            $rowGetUserDon=$resGetUserDon->fetch_assoc();
-            $idUser=$rowGetUserDon['id'];
-        }else{
-            $donarAct=false;
-        }
-    }
-    //echo '<div class="row text-center">'.$cad.'<br>User: '.$idUser.'<br>'.$donarAct.'</div>';
-    if($donarAct==true){
-        $sqlGetStore="SELECT * FROM $tStore WHERE id='$idStore' ";
-        $resGetStore=$con->query($sqlGetStore);
-        $rowGetStore=$resGetStore->fetch_assoc();
+      //echo '<div class="row text-center">'.$cad.'<br>User: '.$idUser.'<br>'.$donarAct.'</div>';
+      if ($donarAct == true) {
+        $sqlGetStore = "SELECT * FROM $tStore WHERE id='$idStore' ";
+        $resGetStore = $con->query($sqlGetStore);
+        $rowGetStore = $resGetStore->fetch_assoc();
         $cad.='<div id="myPrintArea">';
         $cad.='<div class="col-sm-2 ticket">';
-        $cad.='<p class="text-center">"La Concepción Apizaco"<br>Sucursal: '.$rowGetStore['nombre'].'<br>Dirección: '.$rowGetStore['direccion'].'<br>CP: '.$rowGetStore['cp'].'<br>RFC: '.$rowGetStore['rfc'].'<br>Tel: '.$rowGetStore['tel'].'</p>';
+        $cad.='<p class="text-center">"La Concepción Apizaco"<br>Sucursal: ' . $rowGetStore['nombre'] . '<br>Dirección: ' . $rowGetStore['direccion'] . '<br>CP: ' . $rowGetStore['cp'] . '<br>RFC: ' . $rowGetStore['rfc'] . '<br>Tel: ' . $rowGetStore['tel'] . '</p>';
 
         //Obtenemos datos del vendedor y fecha de venta
-        $sqlGetUser="SELECT CONCAT(ap,' ',am,' ',nombre) as nombre FROM $tUser WHERE id='$idUser' ";
-        $resGetUser=$con->query($sqlGetUser);
-        $rowGetUser=$resGetUser->fetch_assoc();
-        $cad.='<p class="text-center">Le atendio: '.$rowGetUser['nombre'].'</br>Fecha: '.$dateNow.'<br>Hora: '.$timeNow.'</p>';
+        $sqlGetUser = "SELECT CONCAT(ap,' ',am,' ',nombre) as nombre FROM $tUser WHERE id='$idUser' ";
+        $resGetUser = $con->query($sqlGetUser);
+        $rowGetUser = $resGetUser->fetch_assoc();
+        $cad.='<p class="text-center">Le atendio: ' . $rowGetUser['nombre'] . '</br>Fecha: ' . $dateNow . '<br>Hora: ' . $timeNow . '</p>';
 
 
-        $sqlCreateInfoSale="INSERT INTO $tSaleInfo (usuario_id, tienda_id, fecha, hora, pago, total, cambio) VALUES ('$idUser', '$idStore', '$dateNow', '$timeNow', '$recibido', '$total', '$cambio' )";
-        if($con->query($sqlCreateInfoSale) === TRUE){
-            $idInfoSale=$con->insert_id;
-            $cad.='<table><thead><tr><th>Producto</th><th>C.U.</th><th>Cant.</th><th>C.T.</th></tr></thead><tbody>';
-            for($i=0; $i<count($_POST['id']); $i++){
-                $idProduct=$_POST['id'][$i];
-                $cant=$_POST['inputCant'][$i];
-                $costoU=$_POST['inputPrecioU'][$i];
-                $costoF=$_POST['inputPrecioF'][$i];
+        $sqlCreateInfoSale = "INSERT INTO $tSaleInfo (usuario_id, tienda_id, fecha, hora, pago, total, cambio) VALUES ('$idUser', '$idStore', '$dateNow', '$timeNow', '$recibido', '$total', '$cambio' )";
+        if ($con->query($sqlCreateInfoSale) === TRUE) {
+          $idInfoSale = $con->insert_id;
+          $cad.='<table><thead><tr><th>Producto</th><th style="padding-left: .5rem;">C.U.</th><th style="padding-left: .5rem;">Cant.</th><th style="padding-left: .5rem;">C.T.</th></tr></thead><tbody>';
+          for ($i = 0; $i < count($_POST['id']); $i++) {
+            $idProduct = $_POST['id'][$i];
+            $cant = $_POST['inputCant'][$i];
+            $costoU = $_POST['inputPrecioU'][$i];
+            $costoF = $_POST['inputPrecioF'][$i];
 
-                $sqlInsertProductSale="INSERT INTO $tSaleProd (producto_id, venta_info_id, cantidad, costo_unitario, costo_total) VALUES ('$idProduct', '$idInfoSale', '$cant', '$costoU', '$costoF') ";
-                if($con->query($sqlInsertProductSale) === TRUE){
-                    $sqlGetCantStock="SELECT cantidad FROM $tStock WHERE producto_id='$idProduct' AND tienda_id='$idStore'";
-                    $resGetCantStock=$con->query($sqlGetCantStock);
-                    if($resGetCantStock->num_rows > 0){
-                        $rowGetCantStock=$resGetCantStock->fetch_assoc();
-                        $cantStock = $rowGetCantStock['cantidad'] - $cant;
-                        $sqlUpdStock="UPDATE $tStock SET cantidad='$cantStock' WHERE producto_id='$idProduct' AND tienda_id='$idStore'  ";
-                        if($con->query($sqlUpdStock) === TRUE){
-                            $sqlGetProduct="SELECT nombre FROM $tProduct WHERE id='$idProduct' ";
-                            $resGetProduct=$con->query($sqlGetProduct);
-                            $rowGetProduct=$resGetProduct->fetch_assoc();
-                            $productName=$rowGetProduct['nombre'];
-                            $cad.='<tr>';
-                            $cad.='<td>'.$productName.'</td>';
-                            $cad.='<td class="text-right">'.$costoU.'</td>';
-                            $cad.='<td class="text-right">'.$cant.'</td>';
-                            $cad.='<td class="text-right">'.$costoF.'</td>';
-                            $cad.='</tr>';
-                            //header("Location: ../form_sales.php");
-                            //echo "true";
-                        }else{
-                            echo "Error al modificar cantidades de almacén.<br>".$con->error;
-                        }
-                    }else{
-                        echo "Error al buscar producto en almacén.<br>".$con->error;
-                    }
-                }else{
-                    echo "Error al crear la lista de productos vendidos.<br>".$con->error;
+            $sqlInsertProductSale = "INSERT INTO $tSaleProd (producto_id, venta_info_id, cantidad, costo_unitario, costo_total) VALUES ('$idProduct', '$idInfoSale', '$cant', '$costoU', '$costoF') ";
+            if ($con->query($sqlInsertProductSale) === TRUE) {
+              $sqlGetCantStock = "SELECT cantidad FROM $tStock WHERE producto_id='$idProduct' AND tienda_id='$idStore'";
+              $resGetCantStock = $con->query($sqlGetCantStock);
+              if ($resGetCantStock->num_rows > 0) {
+                $rowGetCantStock = $resGetCantStock->fetch_assoc();
+                $cantStock = $rowGetCantStock['cantidad'] - $cant;
+                $sqlUpdStock = "UPDATE $tStock SET cantidad='$cantStock' WHERE producto_id='$idProduct' AND tienda_id='$idStore'  ";
+                if ($con->query($sqlUpdStock) === TRUE) {
+                  $sqlGetProduct = "SELECT nombre FROM $tProduct WHERE id='$idProduct' ";
+                  $resGetProduct = $con->query($sqlGetProduct);
+                  $rowGetProduct = $resGetProduct->fetch_assoc();
+                  $productName = $rowGetProduct['nombre'];
+                  $cad.='<tr>';
+                    $cad.='<td>' . $productName . '</td>';
+                    $cad.='<td class="text-right">' . $costoU . '</td>';
+                    $cad.='<td class="text-right">' . $cant . '</td>';
+                    $cad.='<td class="text-right">' . $costoF . '</td>';
+                  $cad.='</tr>';
+                  //header("Location: ../form_sales.php");
+                  //echo "true";
+                } else {
+                  echo "Error al modificar cantidades de almacén.<br>" . $con->error;
                 }
-            }//end for
-        }else{
-            echo "Error al crear información de la venta.<br>".$con->error;
+              } else {
+                echo "Error al buscar producto en almacén.<br>" . $con->error;
+              }
+            } else {
+              echo "Error al crear la lista de productos vendidos.<br>" . $con->error;
+            }
+          }//end for
+        } else {
+          echo "Error al crear información de la venta.<br>" . $con->error;
         }
 
         $cad.='</tbody></table>';
-        $cad.='<p class="text-right">Total: '.$total.'<br>Efectivo: '.$recibido.'<br>Cambio: '.$cambio.'</p>';
-        if($dona=="on") $cad.='<p class="text-center"><b>DONACIÓN</b></p>';
+        $cad.='<p class="text-right">Total: ' . $total . '<br>Efectivo: ' . $recibido . '<br>Cambio: ' . $cambio . '</p>';
+        if ($dona == "on")
+          $cad.='<p class="text-center"><b>DONACIÓN</b></p>';
+        
         $cad.='<p class="text-center">Gracias por su preferencia.</p>';
-        $cad.='</div><div class="col-sm-10"></div>';//Fin col-sm-2
-        $cad.='</div></div>';//Fin área imprime -- Fin row
-        $cad.='<div class="row"><p><a href="javascript:void(0)" id="imprime" class="btn btn-success">Imprime <span class="glyphicon glyphicon-print"></span></a>&nbsp;&nbsp;&nbsp;&nbsp;<a href="../form_sales.php">Cancelar/Atrás</a></p></div>';
+        $cad.='</div><div class="col-sm-10"></div>'; //Fin col-sm-2
+        $cad.='</div></div>'; //Fin área imprime -- Fin row
+        $cad.='<div style="padding-left: 1rem;"><p><a href="javascript:void(0)" id="imprime" class="btn btn-success">Imprime <span class="glyphicon glyphicon-print"></span></a>&nbsp;&nbsp;&nbsp;&nbsp;<a href="../form_sales.php" class="btn btn-default">Atrás</a></p></div>';
 
         echo $cad;
-    }//end else $donarAct
-    else{
+      }//end else $donarAct
+      else {
         $cad.='<div class="col-sm-12 text-center"><h3>No se puede donar ya que el administrador es incorrecto.</h3><br><a href="../form_sales.php">Cancelar/Atrás</a></div></div>';
         echo $cad;
-    }
+      }
+      ?>
 
-?>
-
-<script type="text/javascript">
-    $(document).ready(function() {
-        $('#imprime').click(function() {
+      <script type="text/javascript">
+        $(document).ready(function () {
+          $('#imprime').click(function () {
             $("div#myPrintArea").printArea();
+          });
         });
-    });
-</script>
+      </script>
 
 
-  <footer class="footer">
-    <p >SOTFLUTIONS 2015</p>
-  </footer>
+      <footer class="footer">
+        <p >SOTFLUTIONS 2015</p>
+      </footer>
   </body>
 </html>
