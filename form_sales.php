331f15cc0cacdf7b859b6ec3b0246701877ceef2
LuigiPerezCalzada <luigi.perez.calzada@gmail.com>
2021-08-01 18:34:20 -0500

    LPC: corrección de función producto

--- form_sales.php
+++ form_sales.php
@@ -119,29 +119,30 @@ else {
 
 
 <script type="text/javascript">
+    $(document).ready(function () {
   //Funcion para llenar la DataTable haciendo solo una peticion a la base de datos
-  function product(){
-    $.ajax({
-      type: "POST",
-      data: {idStore: <?= $idStore; ?>},
-      url: "controllers/select_sales_products_json.php",
-      success: function(msg){
-        var msg = jQuery.parseJSON(msg);
-        if(msg.error == 0){
-          alert(msg.dataRes[0].id);
-          (".ventas ").append(msg);
-          $.each(msg.dataRes, function(i, item)){
-            var newRow = '<tr>'
-            +'<td>'+msg.dataRes[i].id+'</td>'
-            +'<td>'+msg.dataRes[i].nombre+'</td>'
-            +'<td>'+msg.dataRes[i].imagen+'</td>'
-            '</tr>';
-            $(newRow).appendTo("#ventas");
-          }
-        }
+      product();
+      function product() {
+          $.ajax({
+              type: "POST",
+              url: "controllers/select_sales_products_json.php",
+              success: function (msg) {
+                  var msg = jQuery.parseJSON(msg);
+                  console.log( msg );
+                  if (msg.error == 0) {
+                      // (".ventas ").append(msg);
+                      $.each(msg.dataRes, function (i, item) {
+                          var newRow = '<tr>' +
+                            '<td>' + msg.dataRes[i].id + '</td>' +
+                            '<td>' + msg.dataRes[i].nombre + '</td>' +
+                            '<td>' + msg.dataRes[i].imagen + '</td>'
+                          '</tr>';
+                          $(newRow).appendTo("#ventas");
+                      })
+                  }
+              }
+          });
       }
-    });
-  }
   
 
    /* $(document).ready(function () {
